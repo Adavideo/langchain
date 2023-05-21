33b1618a9fea2c23bc04063f1a3d46c6571f6ea7
adavideo <adavideo@gmail.com>
2023-05-21 22:15:05 +0200

    Chat with memory

---
+++ 9_chat_with_memory.py
@@ -0,0 +1,28 @@
+import load_keys
+from langchain.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate
+)
+from langchain.chains import ConversationChain
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+
+instructions = "The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know."
+
+prompt = ChatPromptTemplate.from_messages([
+    SystemMessagePromptTemplate.from_template(instructions),
+    MessagesPlaceholder(variable_name="history"),
+    HumanMessagePromptTemplate.from_template("{input}")
+])
+
+llm = ChatOpenAI(temperature=0)
+memory = ConversationBufferMemory(return_messages=True)
+conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm)
+
+input_list = ["Hi there!", "I'm doing well! Just having a conversation with an AI.", "Tell me about yourself."]
+for input in input_list:
+    print(input)
+    result = conversation.predict(input=input)
+    print(result)
